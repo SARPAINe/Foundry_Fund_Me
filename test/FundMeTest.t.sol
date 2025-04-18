335951afa0485dcce4996dd0ac6b57b28ef058c0
Shaharin Ahmed <ahmedshaharin7589@gmail.com>
2025-04-18 16:02:59 +0600

    refactor: refactor fundMe, deploy and testing procedure

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.18;
 
 import {Test, console} from "forge-std/Test.sol";
 import {FundMe} from "../src/FundMe.sol";
+import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 
 contract FundMeTest is Test {
     FundMe fundMe;
@@ -11,7 +12,8 @@ contract FundMeTest is Test {
     function setUp() external {
         // us -> FundMeTest -> FundMe
         // FundMeTest is the deployer of FundMe
-        fundMe = new FundMe();
+        DeployFundMe deployFundMe = new DeployFundMe();
+        fundMe = deployFundMe.run();
     }
 
     function testMinimumDollarIsFive() public view {
@@ -19,7 +21,7 @@ contract FundMeTest is Test {
     }
 
     function testOwnerIsMsgSender() public view {
-        assertEq(fundMe.i_owner(), address(this));
+        assertEq(fundMe.i_owner(), msg.sender);
     }
 
     function testPriceFeedVersionIsAccurate() public view {
